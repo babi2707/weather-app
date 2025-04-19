38c16d16197f9ed694fc7af981f16df55dae9dd8
babi2707 <90732683+babi2707@users.noreply.github.com>
2025-04-19 14:44:22 +0100

    backend conection with api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+    setWeatherData(null);
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=825e8f385a08c06f3d6978f7dc4f8344`
+      );
+
+      setWeatherData(response.data);
+    } catch (erro) {
+      if (city == "") {
+        setError("Please enter the city!");
+      } else {
+        setError("City not found");
+      }
+    }
+  };
+
   return (
     <>
-      <body>
-        <div class="weather-search-container">
-          <h1 className="heading-main">Weather App</h1>
-          <form id="weatherForm">
-            <input type="text" id="cityInput" placeholder="Enter city name" />
-            <button type="submit">Search</button>
-          </form>
-          <div id="error" class="error"></div>
-          <div id="weatherResult" class="weather-result"></div>
-          <div className="messages"></div>
+      <div className="weather-search-container">
+        <h1 className="heading-main">Weather App</h1>
+        <form id="weatherForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="cityInput"
+            value={city}
+            onChange={handleChange}
+            placeholder="Enter city name"
+          />
+          <button type="submit">Search</button>
+        </form>
+        <div id="error" className="error"></div>
+        <div id="weatherResult" className="weather-result"></div>
+        <div className="messages">
+          {error && <p className="error">{error}</p>}
+          {weatherData && (
+            <div className="weather-result">
+              <h2>{weatherData.name}</h2>
+              <p>
+                Temperature: {(weatherData.main.temp - 273.15).toFixed(1)} °C
+              </p>
+              <p>Weather: {weatherData.weather[0].description}</p>
+              <p>Country: {weatherData.sys.country}</p>
+            </div>
+          )}
         </div>
-      </body>
+      </div>
     </>
   );
 }
